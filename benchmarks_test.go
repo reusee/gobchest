bb8ae132c39d39a813eb7aeb019e05df451c01bf
reus <reusee@gmail.com>
2014-11-13 23:55:20 +0800

    add benchmarks

---
+++ benchmarks_test.go
@@ -0,0 +1,40 @@
+package store
+
+import "testing"
+
+func BenchmarkSet(b *testing.B) {
+	server, err := NewServer(randomAddr(), randomFilePath())
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer server.Close()
+	client, err := NewClient(server.addr)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer client.Close()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		client.Set("foo", i)
+	}
+}
+
+func BenchmarkGet(b *testing.B) {
+	server, err := NewServer(randomAddr(), randomFilePath())
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer server.Close()
+	client, err := NewClient(server.addr)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer client.Close()
+	client.Set("foo", "foo")
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		client.Get("foo")
+	}
+}
